b4f9a955b9e394b0855b9263a61cf77fa2ef2f90
luangf <luangf222@gmail.com>
2023-01-19 20:40:38 -0300

    att

--- meuprojetojsf/src/main/java/br/com/cursojsf/LancamentoBean.java
+++ meuprojetojsf/src/main/java/br/com/cursojsf/LancamentoBean.java
@@ -30,7 +30,7 @@ public class LancamentoBean {
 		Pessoa pessoaUser = (Pessoa) externalContext.getSessionMap().get("usuarioLogado");
 		lancamento.setUsuario(pessoaUser);
 
-		daoGeneric.salvar(lancamento);
+		lancamento=daoGeneric.merge(lancamento);
 		carregarLancamentos();
 		
 		return "";
