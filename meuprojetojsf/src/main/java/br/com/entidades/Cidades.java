fea7a2d66910188c65e9ac8652237d791e2220ab
luangf <luangf222@gmail.com>
2023-01-23 17:21:54 -0300

    Criando entidades de cidades e estados, dps de acrescentar vários registros de vários estados e cidades no banco de dados. Tudo isso com o intuito de fazer Combos dependentes no JSF(front-end) para selecionar a cidade e estado no form

---
+++ meuprojetojsf/src/main/java/br/com/entidades/Cidades.java
@@ -0,0 +1,72 @@
+package br.com.entidades;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Cidades implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String nome;
+
+	// fetch = FetchType.EAGER: quando carrega cidade carrega estado tbm
+	// cascade = CascadeType.REFRESH: quando quero resgatar/atualizar no BD, eu
+	// quero fazer isso em cascata
+	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.REFRESH)
+	private Estados estado;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Estados getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Estados estado) {
+		this.estado = estado;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cidades other = (Cidades) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
---
+++ meuprojetojsf/src/main/java/br/com/entidades/Estados.java
@@ -0,0 +1,46 @@
+package br.com.entidades;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Estados implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String nome;
+	private String sigla;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSigla() {
+		return sigla;
+	}
+
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+
+}
